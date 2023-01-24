1204093c2f01177f4120b86c57e7c75a03b2ea24
WaifuFag <70859508+Waifu-Amk@users.noreply.github.com>
2023-01-24 13:21:30 -0600

    New changes

---
+++ Catalogo/Catalogo.Core/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogo.Core
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public List <Producto> Producto { get; set; }
+
+    }
+}
---
+++ Catalogo/Catalogo.DataAccess/CatalogoContext.cs
@@ -0,0 +1,25 @@
+﻿using Catalogo.Core;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogo.DataAccess
+{
+    public class CatalogoContext :IdentityDbContext
+    {
+        public virtual DbSet<Categoria> Categorias { get; set; }
+
+        public virtual DbSet<Marca> Marcas { get; set; }
+
+        public virtual DbSet<Producto> Productos { get; set; }
+
+        public CatalogoContext(DbContextOptions<CatalogoContext> options ) : base(options)
+        {
+
+        }
+    }
+}
